14357ac2509b9c81eb4e07928827f5ff07bd581a
S. Amoateng <soppongamoateng@gmail.com>
2021-01-16 23:56:11 +0000

    create index.php

---
+++ index.php
@@ -0,0 +1,132 @@
+<?php
+echo '<body>
+	<div class="w3-top">
+	  <div class="w3-bar w3-blue w3-card w3-left-align w3-large">
+	    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-large w3-blue" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">Dawurobo</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Sell Your Bed Space</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Book Guest House</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Sign up</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Login</a>
+	  </div>
+
+	  
+	  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Dawurobo</a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Sell Your Bed Space</a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Book Guest House</a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
+	  </div>
+	</div>
+
+	<header class="w3-container w3-white w3-center" style="padding:128px 16px">
+	  <h1 class="w3-margin w3-jumbo">Get what you need</h1>
+	  <p class="w3-xlarge">Swap a bed space or get one</p>
+
+	  	<form>
+  			<input type="text" name="search" placeholder="Type what you need...">
+		</form>
+
+
+
+	  <!-- <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Get Started</button> -->
+	</header>
+	
+	
+		<div class="container">
+	  <h2>On Hostels</h2>
+	 
+	  <div class="card left-card" style="width:400px">
+	    <img class="card-img-top" src="https://www.w3schools.com/w3images/img_avatar1.png" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Tek Credit Hostel</h4>
+	      <p class="card-text">Tek Credit Hostel is on-campus.</p>
+	      <a href="#" class="btn btn-primary stretched-link">Available bed spaces</a>
+	    </div>
+	  </div>
+
+	  <div class="card left-card" style="width:400px">
+	    <img class="card-img-top" src="https://www.w3schools.com/w3images/img_avatar1.png" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Hall 7</h4>
+	      <p class="card-text">Hall 7 is on-campus.</p>
+	      <a href="#" class="btn btn-primary stretched-link">Available bed spaces</a>
+	    </div>
+	  </div>
+
+	   <div class="card left-card" style="width:400px">
+	    <img class="card-img-top" src="https://www.w3schools.com/w3images/img_avatar1.png" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">SRC Hostel</h4>
+	      <p class="card-text">SRC Hostel is on-campus.</p>
+	      <a href="#" class="btn btn-primary stretched-link">Available bed spaces</a>
+	    </div>
+
+	  </div>
+	</div>
+	  		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	 <div class="container">
+
+
+	  <h2>Off-Hostels</h2>
+	 
+	  <div class="card left-card" style="width:400px">
+	    <img class="card-img-top" src="https://www.w3schools.com/w3images/img_avatar1.png" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Canam Hall</h4>
+	      <p class="card-text">Canam Hall is off-campus.</p>
+	      <a href="#" class="btn btn-primary stretched-link">Available bed spaces</a>
+	    </div>
+	  </div>
+
+	  <div class="card left-card" style="width:400px">
+	    <img class="card-img-top" src="https://www.w3schools.com/w3images/img_avatar1.png" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Victory Towers</h4>
+	      <p class="card-text">Victory Towers is off-campus.</p>
+	      <a href="#" class="btn btn-primary stretched-link">Available bed spaces</a>
+	    </div>
+	  </div>
+
+	   <div class="card left-card" style="width:400px">
+	    <img class="card-img-top" src="https://www.w3schools.com/w3images/img_avatar1.png" alt="Card image" style="width:100%">
+	    <div class="card-body">
+	      <h4 class="card-title">Frontline Inn</h4>
+	      <p class="card-text">Frontline Inn is off-campus.</p>
+	      <a href="#" class="btn btn-primary stretched-link">Available bed spaces</a>
+	    </div>
+
+
+
+	  </div>
+ <br><br><br><br>
+	 <br><br><br><br> <br><br><br><br>
+	 <br><br><br><br> <br><br><br><br> <br><br><br><br>
+
+		</div>
+
+</body>'
+?>
+<<html>
+
+<head>
+	<title>Dawurobo</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="w3.css">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	
+
+</head
+
+</html>
