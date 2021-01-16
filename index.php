fd9509b5f0263af2c08484ad6eb40465a151865a
S. Amoateng <soppongamoateng@gmail.com>
2021-01-16 13:08:51 +0000

    update index.php

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
 <?php
 echo '<body>
-<h1>what now?</h1>
+	<div class="w3-top">
+	  <div class="w3-bar w3-white w3-card w3-left-align w3-large">
+	    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">Dawurobo</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Categories</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Post Ad</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Sign up</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Login</a>
+	  </div>
 
+	  
+<!-- 	  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
+	  </div> -->
+	</div>
+
+	<header class="w3-container w3-white w3-center" style="padding:128px 16px">
+	  <h1 class="w3-margin w3-jumbo">Get what you need</h1>
+	  <p class="w3-xlarge">Swap a bed space or get one</p>
+
+	  	<form>
+  			<input type="text" name="search" placeholder="Type what you need...">
+		</form>
+
+
+
+	  <!-- <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Get Started</button> -->
+	</header>
 
 </body>'
 ?>
