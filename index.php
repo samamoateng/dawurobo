6a9d087d52bcecf98aa341447cda99de1478d2c1
S. Amoateng <soppongamoateng@gmail.com>
2021-01-16 23:21:36 +0000

    update index.php

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 echo '<body>
 	<div class="w3-top">
-	  <div class="w3-bar w3-white w3-card w3-left-align w3-large">
-	    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+	  <div class="w3-bar w3-blue w3-card w3-left-align w3-large">
+	    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-large w3-blue" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
 	    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">Dawurobo</a>
-	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Categories</a>
-	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Post Ad</a>
-	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Sign up</a>
-	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Login</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Sell Your Bed Space</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Book Guest House</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Sign up</a>
+	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Login</a>
 	  </div>
 
 	  
-<!-- 	  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
+	  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">D</a>
 	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
 	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
 	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
-	  </div> -->
+	  </div>
 	</div>
 
 	<header class="w3-container w3-white w3-center" style="padding:128px 16px">
