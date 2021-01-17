2ca073c413ecc47141635efb1caf4c26fe9f225a
S. Amoateng <soppongamoateng@gmail.com>
2021-01-17 00:14:44 +0000

    update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo '<body>
 	<div class="w3-top">
 	  <div class="w3-bar w3-blue w3-card w3-left-align w3-large">
 	    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-large w3-blue" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-	    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">Dawurobo</a>
+	    <a href="#" class="w3-bar-item w3-button w3-padding-large">Dawurobo</a>
 	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Sell Your Bed Space</a>
 	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Book Guest House</a>
 	    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large">Sign up</a>
